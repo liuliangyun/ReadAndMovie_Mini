03a2eb4e39ae4b440aa6fccd35280f559b165723
lyliu <lyliu@thoughtworks.com>
2019-10-17 14:58:26 +0800

    [lly] practice handle event

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -13,7 +13,7 @@ Page({
    * call服务端请求数据并setData
    */
   onLoad: function (options) {
-    console.log("onLoad");
+    console.log("Posts data is load");
     var posts_content = [{
       date: "Oct 11 2019",
       title: "正是虾肥蟹壮时",
@@ -39,48 +39,41 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-    console.log("onReady");
   },
 
   /**
    * Lifecycle function--Called when page show
    */
   onShow: function () {
-    console.log("onShow");
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
   onHide: function () {
-    console.log("onHide");
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
   onUnload: function () {
-    console.log("onUnload");
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
   onPullDownRefresh: function () {
-    console.log("pull down");
   },
 
   /**
    * Called when page reach bottom
    */
   onReachBottom: function () {
-    console.log("reach bottom");
   },
 
   /**
    * Called when user click on the top right corner to share
    */
   onShareAppMessage: function () {
-    console.log("share");
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,21 @@
+// 实现用户交互：产生事件 捕捉事件 回调函数 处理事件
 Page({
+  onTap: function() {
+    // 可以back，执行onHide
+    wx.navigateTo({
+      url: "../posts/posts"
+    });
+    // 不可以back，执行onUnload
+    // wx.redirectTo({
+    //   url: "../posts/posts"
+    // });
+  },
   
+  onHide: function () {
+    console.log("Welcome page is hide");
+  },
+
+  onUnload: function () {
+    console.log("Welcome page is unload");
+  },
 })
