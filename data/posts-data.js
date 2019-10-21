bf148e9b5d0997514e75c41744b377679affad01
lyliu <lyliu@thoughtworks.com>
2019-10-21 16:47:43 +0800

    [lly] export and import posts data

---
+++ data/posts-data.js
@@ -0,0 +1,59 @@
+ var local_database = [{
+     date: "Oct 11 2019",
+     title: "正是虾肥蟹壮时",
+     imgSrc: "/images/post/crab.png",
+     avatar: "/images/avatar.png",
+     content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满”。",
+     reading: "112",
+     collection: "96"
+   }, {
+     date: "Oct 14 2019",
+     title: "比利·林恩的中场故事",
+     imgSrc: "/images/post/bl.png",
+     avatar: "/images/avatar.png",
+     content: "李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+     reading: "62",
+     collection: "92"
+   },
+   {
+     //按住alt + shift + F 可以格式化代码样式
+     date: "Nov 12 2016",
+     title: "当我们在谈论经济学时，我们在谈论什么?",
+     imgSrc: "/images/post/sls.jpg",
+     avatar: "/images/avatar.png",
+     content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+     reading: "62",
+     collection: "92"
+   },
+   {
+     date: "Nov 20 2016",
+     title: "微信·小程序开发工具安装指南",
+     imgSrc: "/images/post/xiaolong.jpg",
+     avatar: "/images/avatar.png",
+     content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+     reading: "102",
+     collection: "92"
+   },
+   {
+     date: "Nov 20 2016",
+     title: "从视觉到触觉 这款VR手套能给你真实触感",
+     imgSrc: "/images/post/vr.png",
+     avatar: "../../images/avatar.png",
+     content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+     reading: "102",
+     collection: "26"
+   },
+   {
+     date: "Nov 20 2016",
+     title: "爱奇艺创维开展战略合作，合力布局开放娱乐生态",
+     imgSrc: "/images/iqiyi.png",
+     avatar: "../../images/avatar.png",
+     content: "爱奇艺和创维分别作为国内领先的在线视频品牌",
+     reading: "96",
+     collection: "26"
+   },
+ ];
+
+ module.exports = {
+   postList: local_database
+ }
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,38 +1,16 @@
+// 只能用相对路径
+var postsData = require('../../data/posts-data.js')
+
 // pages/posts/posts.js
 Page({
-
-  /**
-   * Page initial data
-   */
   data: {
-
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   * call服务端请求数据并setData
-   */
   onLoad: function (options) {
+    this.setData({ 
+      posts_key: postsData.postList 
+    });
     console.log("Posts data is load");
-    var posts_content = [{
-      date: "Oct 11 2019",
-      title: "正是虾肥蟹壮时",
-      background: "/images/post/crab.png",
-      avatar: "/images/avatar.png",
-      content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满”。",
-      reading: "112",
-      collection: "96"
-    }, {
-      date: "Oct 14 2019",
-      title: "比利·林恩的中场故事",
-      background: "/images/post/bl.png",
-      avatar: "/images/avatar.png",
-      content: "李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-      reading: "62",
-      collection: "92"
-    }];
-    // setData接收参数为JS对象，而这里posts_content只是一个数组，应写成{posts_content: posts_content}，或ES6简写为{posts_content}
-    this.setData({posts_content});
   }
   
 })
