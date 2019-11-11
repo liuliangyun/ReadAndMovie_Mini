b94b99176f7d92705475c58e135957ffbd9280f3
lyliu <lyliu@thoughtworks.com>
2019-11-11 11:13:47 +0800

    [lly] implement view detail page function on swiper

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -18,6 +18,22 @@ Page({
     wx.navigateTo({
       url: "./post-detail/post-detail?id=" +postId
     })
+  },
+
+  // onSwiperItemTap: function (event) {
+  //   var postId = event.currentTarget.dataset.postid;
+  //   wx.navigateTo({
+  //     url: "./post-detail/post-detail?id=" + postId
+  //   })
+  // },
+
+  onSwiperTap: function (event) {
+    // target 指的是当前点击的组件；而 currentTarget 指的是事件捕获的组件
+    // target 这里指的是image；而 currentTarget 这里指的是swiper
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: "./post-detail/post-detail?id=" + postId
+    })
   }
   
 })
